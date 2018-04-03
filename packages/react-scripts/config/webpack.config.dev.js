827f3c5b6d5523e5c61de1c4c5b1adfa7331e51a
Maayan Glikser <maayan@glikm.com>
2018-04-03 16:33:06 +0300

    remove alias for tests as it is not needed

--- packages/react-scripts/config/webpack.config.dev.js
+++ packages/react-scripts/config/webpack.config.dev.js
@@ -104,8 +104,7 @@ module.exports = {
       // @remove-on-eject-end
       // Support React Native Web
       // https://www.smashingmagazine.com/2016/08/a-glimpse-into-the-future-with-react-native-for-web/
-      'react-native': 'react-native-web',
-      tests: paths.testSrc,
+      'react-native': 'react-native-web'
     },
     plugins: [
       // Prevents users from importing files from outside of src/ (or node_modules/).
